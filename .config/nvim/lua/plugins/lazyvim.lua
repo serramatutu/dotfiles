ccb258017d0b27773239cb4a8c7ceffcb86faa7f
serramatutu <lucas.paes@dbtlabs.com>
2024-11-20 18:28:57 +0100

    Update nvim

--- .config/nvim/lua/plugins/lazyvim.lua
+++ .config/nvim/lua/plugins/lazyvim.lua
@@ -5,4 +5,12 @@ return {
       colorscheme = "ayu",
     },
   },
+  -- TODO: this is only necessary because lazynvim doesn't install this
+  -- for some reason
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {},
+  },
 }
