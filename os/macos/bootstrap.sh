e73e742b6f504d502443b205d356df1e167b1f21
serramatutu <lucas.paes@dbtlabs.com>
2025-02-13 12:07:14 +0100

    Faster key repeate on macos

--- os/macos/bootstrap.sh
+++ os/macos/bootstrap.sh
@@ -10,4 +10,7 @@ skhd --start-service
 yabai --install-service
 yabai --start-service
 
+defaults write -g InitialKeyRepeat -int 13
+defaults write -g KeyRepeat -int 2
+
 ln -s $DOTFILES/.config/alacritty/macos.toml $DOTFILES/.config/alacritty/os.toml
