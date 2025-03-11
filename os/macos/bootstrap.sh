abdad23e56fb46e6c3b7e4ddba355dcad26073c6
serramatutu <lucas.paes@dbtlabs.com>
2025-03-11 14:17:29 +0100

    Make MacOS dock snappier

--- os/macos/bootstrap.sh
+++ os/macos/bootstrap.sh
@@ -12,5 +12,7 @@ yabai --start-service
 
 defaults write -g InitialKeyRepeat -int 13
 defaults write -g KeyRepeat -int 2
+defaults write com.apple.dock autohide-delay -float 0 
+defaults write com.apple.dock autohide-time-modifier -float 0.5
 
 ln -s $DOTFILES/.config/alacritty/macos.toml $DOTFILES/.config/alacritty/os.toml
