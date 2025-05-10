463edc3dcdcfc3b44f4d70d0b103454630c2bb24
serramatutu <lucasvvop@hotmail.com>
2025-05-10 22:57:42 +0200

    Migrate from X11 to Wayland (i3 -> Sway)
    X11 performance is so trash omg

--- os/linux/zshload.zsh
+++ os/linux/zshload.zsh
@@ -1,11 +1,1 @@
 source "$HOME/.asdf/asdf.sh"
-
-display_mobile() {
-  xrandr --output eDP1 --below HDMI1 --auto
-}
-
-display_docked() {
-  xrandr \
-    --output eDP1 --below HDMI1 --auto \
-    --output HDMI1 --primary --mode 2560x1440 --rate 60 
-}
