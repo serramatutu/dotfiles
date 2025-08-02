bdece2e3c7c1f6d3d2c41f7c6afff98399e28852
serramatutu <lucasvvop@hotmail.com>
2025-08-02 12:27:46 +0200

    Fix leader key nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,7 @@
+-- <Space> as leader (needs to be done before lazy)
+vim.g.mapleader = " "
 require("config.lazy")
+
 require("config.options")
 require("config.autocmds")
 require("config.keymaps")
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,9 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- <Space> as leader
-vim.g.mapleader = " "
-
 -- display relative line numbers
 vim.wo.number = true
 vim.wo.relativenumber = true
