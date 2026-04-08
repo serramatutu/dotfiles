7c8d451cb8b75abe9e3a44344899d6a4954233ac
serramatutu <lucas.paes@dbtlabs.com>
2026-04-08 15:56:50 -0700

    Replace telescope fzy with fzf

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,7 +4,10 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope-ui-select.nvim",
-      "nvim-telescope/telescope-fzy-native.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install",
+      },
       {
         "nvim-telescope/telescope-live-grep-args.nvim",
         -- This will not install any breaking changes.
@@ -23,7 +26,7 @@ return {
       })
       telescope.load_extension("ui-select")
       telescope.load_extension("live_grep_args")
-      telescope.load_extension("fzy_native")
+      telescope.load_extension("fzf")
     end,
     keys = {
       -- find
