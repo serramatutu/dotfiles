460169a7037a623ed905ee270285ef7c84b0779d
serramatutu <serramatutu@pm.me>
2026-09-07 18:31:52 +0200

    Better themes

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,10 +6,6 @@
 vim.wo.number = true
 vim.wo.relativenumber = true
 
--- highlight cursor
-vim.wo.cursorline = true
-vim.wo.cursorcolumn = true
-
 -- colors
 vim.opt.guicursor = "n-v-c-sm:block-Cursor,i-ci-ve:ver25-Cursor,r-cr-o:hor20"
 
